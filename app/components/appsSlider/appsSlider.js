1e4aacfb9122a0dee9757e92557ea1bdc7e0e766
Rodrigo Zan de Oliveira <rzanoliveira@gmail.com>
2024-10-21 22:25:57 -0300

    Convert ppps cards from 'div' to 'button';

--- app/components/appsSlider/appsSlider.js
+++ app/components/appsSlider/appsSlider.js
@@ -6,17 +6,21 @@ export function appsSlider() {
   slider.setAttribute('class', 'slider');
   slider.setAttribute('id', 'slider__road-apps');
 
-  const sliderItemCard01 = document.createElement('div');
+  const sliderItemCard01 = document.createElement('button');
   sliderItemCard01.setAttribute('class', 'slider__item-card');
+  sliderItemCard01.setAttribute('href', '#');
 
-  const sliderItemCard02 = document.createElement('div');
+  const sliderItemCard02 = document.createElement('button');
   sliderItemCard02.setAttribute('class', 'slider__item-card');
+  sliderItemCard02.setAttribute('href', '#');
 
-  const sliderItemCard03 = document.createElement('div');
+  const sliderItemCard03 = document.createElement('button');
   sliderItemCard03.setAttribute('class', 'slider__item-card');
+  sliderItemCard03.setAttribute('href', '#');
 
-  const sliderItemCard04 = document.createElement('div');
+  const sliderItemCard04 = document.createElement('button');
   sliderItemCard04.setAttribute('class', 'slider__item-card');
+  sliderItemCard04.setAttribute('href', '#');
 
   // APPEND CHILD
   slider.appendChild(sliderItemCard01);
